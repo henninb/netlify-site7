2fd6d24d1324ddc3bd3ecebd850f05e413f12a38
Brian Henning <brian.henning@humansecurity.com>
2022-09-30 09:48:03 -0500

    wip

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Hello World - Netlify Site4
+          Hello React - Netlify Site7
           change
         </p>
       </header>
